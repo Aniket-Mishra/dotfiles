1b1b1357c864b40688f8447bbb4d89fd5de71afe
Aniket-Mishra <anmishraofficial@gmail.com>
2025-08-04 01:20:39 +0530

    Adding download organiser - rs

---
+++ rust_tools/download_organizer/src/main.rs
@@ -0,0 +1,75 @@
+use chrono::{DateTime, Local};
+use std::fs;
+use std::io;
+use std::path::Path;
+
+fn main() -> io::Result<()> {
+    let downloads = dirs::download_dir().expect("Could not find Downloads directory");
+
+    let rules = vec![
+        (vec!["jpg", "png", "webp", "gif"], "images"),
+        (vec!["pdf", "docx", "txt", "xlsx"], "documents"),
+        (vec!["dmg", "pkg", "app"], "apps"),
+    ];
+
+    // not recursive
+    for entry in fs::read_dir(&downloads)? {
+        let entry = entry?;
+        let path = entry.path();
+        if !path.is_file() {
+            continue;
+        }
+        let mut moved = false;
+        if let Some(ext) = path
+            .extension()
+            .and_then(|e| e.to_str())
+            .map(|e| e.to_lowercase())
+        {
+            for (extensions, folder) in &rules {
+                if extensions.contains(&ext.as_str()) {
+                    move_and_rename_file(&path, &downloads, folder)?;
+                    moved = true;
+                    break;
+                }
+            }
+        }
+        // misc for all else. Folders are untouched cuz I make folders in downloads.
+        if !moved {
+            move_and_rename_file(&path, &downloads, "misc")?;
+        }
+    }
+
+    Ok(())
+}
+
+fn move_and_rename_file(path: &Path, downloads: &Path, folder: &str) -> io::Result<()> {
+    let dest_dir = downloads.join(folder);
+    fs::create_dir_all(&dest_dir)?;
+
+    let metadata = fs::metadata(&path)?;
+    let created = metadata
+        .created()
+        .or_else(|_| metadata.modified()) // Fallback
+        .unwrap_or_else(|_| std::time::SystemTime::now());
+    let datetime: DateTime<Local> = created.into();
+    let filename = path.file_stem().unwrap().to_string_lossy();
+    let ext = path.extension().and_then(|e| e.to_str()).unwrap_or("");
+    let new_name = format!(
+        "{}_{}.{}",
+        filename,
+        datetime.format("%Y-%m-%d_%H-%M-%S"),
+        ext
+    );
+    let new_path = dest_dir.join(&new_name);
+
+    // Print what file is moved, where, and its new name
+    println!(
+        "Moving file {} to folder '{}' as {}",
+        path.display(),
+        folder,
+        new_name
+    );
+
+    fs::rename(&path, &new_path)?;
+    Ok(())
+}
